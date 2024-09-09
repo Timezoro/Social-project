0c29ea07614d4640f32014e5d353e9b9acde515e
Borjuu Pracha <alexinajew@gmail.com>
2024-09-09 22:37:42 +0700

    change message

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Detail from "./components/message/detail/Detail";
+import Chat from "./components/message/chat/Chat";
+import List from "./components/message/list/List";
+
 const App = () => {
   return (
     <div className='container'>
-      
+      <List />
+      <Chat />
+      <Detail />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
