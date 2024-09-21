77fdc600e3ee93fee795b8be1714517cf168f04e
Borjuu Pracha <alexinajew@gmail.com>
2024-09-21 17:05:25 +0700

    done all raw demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Detail from "./components/message/detail/Detail";
 import Chat from "./components/message/chat/Chat";
 import List from "./components/message/list/List";
-
+import Login from "./components/login/login";
+import Notification from "./components/notification/Notification";
 
 const App = () => {
+  
+  const user = true;
+
   return (
-    <div className='container'>
-      <List />
-      <Chat />
-      <Detail />
+    <div className='container'> 
+
+      {
+        user ? (
+        <>
+
+          <List />
+          <Chat />
+          <Detail /> 
+
+        </>          
+      ) : (
+        <Login />
+      )}
+
+      <Notification />
+
     </div>
   );
 }
