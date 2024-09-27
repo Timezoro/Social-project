18bbdce746387ebb6fcb65f2db35bc9fea191c23
Borjuu Pracha <alexinajew@gmail.com>
2024-09-27 21:21:26 +0700

    firebase login/out

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,29 @@ import Login from "./components/login/login";
 import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 const App = () => {
   
-  // login page use true or false
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      fetchUserInfo(user?.uid);
     });
 
     return () => {
       unSub();
     };
-  }, []);
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className='container'> 
 
       {
-        user ? (
+        currentUser ? (
         <>
 
           <List />
--- src/components/message/list/userinfo/Userinfo.jsx
+++ src/components/message/list/userinfo/Userinfo.jsx
@@ -1,11 +1,15 @@
 import "./userinfo.css";
+import { useUserStore } from "../../../../lib/userStore";
 
 const Userinfo = () => {
+
+  const { currentUser } = useUserStore();
+
   return (
     <div className="userinfo">
         <div className="user">
-            <img src="./avatar.png" alt="User Avatar" />
-            <h2>Cillian</h2>
+            <img src={currentUser.avatar || "./avatar.png"} alt="User Avatar" />
+            <h2>{currentUser.username}</h2>
         </div>
         <div className="icon">
             <img src="./more.png" alt="More Options" />
