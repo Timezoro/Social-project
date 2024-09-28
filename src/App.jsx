824dea365bdcdf35ae9b2d44d423bdf95b7f2f60
Timezoro <81484071+Timezoro@users.noreply.github.com>
2024-09-28 19:14:48 +0700

    Loginpage and registerpage finish adding router npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserProvider } from "./context/UserContext";
 import Detail from "./components/message/detail/Detail";
 import Chat from "./components/message/chat/chat";
 import List from "./components/message/list/List";
 import Login from "./components/login/Loginpage";
+import Register from "./components/login/Registerpage";
 
 const App = () => {
   return (
-    <div>
-      <Login />
-      {/* <List />
-      <Chat />
-      <Detail /> */}
-    </div>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
 
