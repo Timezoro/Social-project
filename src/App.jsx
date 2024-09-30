5b31c77b6b97204f6a578b9f9aa958750694addf
Timezoro <pongpiriyatime@gmail.com>
2024-09-30 23:48:47 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Chat from "./components/message/chat/Chat";
 import List from "./components/message/list/List";
 import Login from "./components/login/login";
 import Notification from "./components/notification/Notification";
+import CreateSite from "./components/mainsite/createPost/createSite";
+import { UserProvider } from "./context/UserContext";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
 import { useUserStore } from "./lib/userStore";
@@ -26,24 +28,23 @@ const App = () => {
   if (isLoading) return <div className="loading">Loading...</div>
 
   return (
-    <div className=' container '> 
+    <UserProvider>
+        <CreateSite />
+      {/* <div className="container">
+        {currentUser ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (
+          <Login />
+        )}
+        <Notification />
+      </div> */}
+    </UserProvider>
+      
 
-      {
-        currentUser ? (
-        <>
-
-          <List />
-          <Chat />
-          <Detail /> 
-
-        </>          
-      ) : (
-        <Login />
-      )}
-
-      <Notification />
-
-    </div>
   );
 }
 
